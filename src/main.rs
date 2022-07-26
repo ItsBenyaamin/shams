44a7e0f47522083f405129091e9a6284136e6fd2
Benyamin Eskandari <me@benyaamin.com>
2022-07-26 19:36:00 +0430

    add internet check for fetch occasions

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,50 @@
-mod cache;
-mod calendar;
-mod consts;
-mod output;
+extern crate ptime;
+
 mod shams;
+mod utils;
 
-use output::printer::printer::*;
-use shams::*;
+use std::io::{stdout, Write};
+use std::thread::sleep;
+use std::time::Duration;
+use colored_truecolor::Colorize;
+use utils::config::Config;
+use utils::server;
+use utils::config;
+use shams::calendar;
+use crate::calendar::Calendar;
+use crate::shams::occasions::{Occasion, Occasions};
 
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
+type Result = std::result::Result<(), Box<dyn std::error::Error>>;
 
 #[tokio::main]
-async fn main() -> crate::Result<()> {
-    let args: Vec<String> = std::env::args().collect();
+async fn main() -> Result{
+    let config = Config::load();
+    let persian_calendar = ptime::now();
 
-    if args.len() == 1 {
-        go_with_cache().await?;
-    } else if args.len() > 2 {
-        println!("too many args! see 'shams -h.");
-    } else {
-        match args[1].as_str() {
-            "-h" | "--help" => print_help(),
-            "-u" | "--update" => request().await?,
-            "-a" | "--about" => print_about(),
-            _ => print_help(),
+    if !config.is_occasions_fetched {
+        check_internet_for_occasions(persian_calendar.tm_year as u16).await;
+    }else if config.fetched_occasions_year.is_some() {
+        if config.fetched_occasions_year.unwrap() != persian_calendar.tm_year as u16 {
+            check_internet_for_occasions(persian_calendar.tm_year as u16).await;
         }
     }
 
     Ok(())
+}
+
+async fn check_internet_for_occasions(year: u16) {
+    let mut occasions: Option<Vec<Occasions>> = None;
+    let online = online::check(Some(3)).await;
+    if online.is_ok() {
+        occasions = server::fetch_occasions(year).await;
+        handle_occasions(occasions);
+    } else {
+        println!("{}", "Internet is not available for fetch Occasions from server!".red());
+    }
+}
+
+fn handle_occasions(_occasions: Option<Vec<Occasions>>) {
+    if let Some(occasions) = _occasions {
+        //TODO handle this
+    }
 }
