4b3b7f2cf874b572b43718bc2b5ece25d35a573e
Benyamin Eskandari <me@benyaamin.com>
2022-07-27 15:59:23 +0430

    add save & to_string to occasions

--- src/shams/occasions.rs
+++ src/shams/occasions.rs
@@ -1,4 +1,5 @@
 use serde::{Serialize, Deserialize};
+use crate::{constants, storage_helper};
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct Occasions {
@@ -10,4 +11,26 @@ pub struct Occasions {
 pub struct Occasion {
     pub title: String,
     pub is_holiday: bool
+}
+
+impl Occasions {
+
+    pub fn save_occasions(&self) -> Option<()> {
+        if let Some(occasions) = _occasions {
+            let json_string = serde_json::to_string(&occasions).unwrap();
+            storage_helper::write_to_file(constants::OCCASIONS_FILE_NAME, json_string).await;
+            return Some(())
+        }
+
+        None
+    }
+
+}
+
+impl ToString for Occasions {
+
+    fn to_string(&self) -> String {
+        serde_json::to_string(&self).unwrap()
+    }
+
 }
