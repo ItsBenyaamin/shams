8d7bd0c6bbb133a1111a0647aeed00435f039ca8
Benyamin Eskandari <me@benyaamin.com>
2022-07-29 15:22:19 +0430

    some cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,9 @@ extern crate ptime;
 mod shams;
 use shams::calendar;
 
-type Result = std::result::Result<(), Box<dyn std::error::Error>>;
 
-fn main() -> Result {
+fn main() {
     let persian_calendar = ptime::now();
     let mut calendar = calendar::Calendar::new(&persian_calendar);
     calendar::print_calendar(&mut calendar);
-    Ok(())
 }
